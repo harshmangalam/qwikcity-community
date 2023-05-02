57883b9ee94307d8b051752e66b7aaaac880dac8
harshmangalam <harshdev8218@gmail.com>
2023-05-02 21:24:23 +0530

    added validations

--- src/routes/(auth)/signup/index.tsx
+++ src/routes/(auth)/signup/index.tsx
@@ -1,17 +1,33 @@
-import { Link } from "@builder.io/qwik-city";
+import { component$ } from "@builder.io/qwik";
+import { Form, Link, routeAction$, z, zod$ } from "@builder.io/qwik-city";
 
-export default function Signup() {
+export const useSignup = routeAction$(
+  async (form, event) => {},
+  zod$({
+    name: z.string().nonempty("Name must be required"),
+    email: z.string().email("Email must be valid email address"),
+
+    password: z.string().nonempty("Password must be required"),
+  })
+);
+export default component$(() => {
+  const action = useSignup();
+
   return (
     <div class={"min-h-screen h-full grid place-items-center bg-gray-100"}>
       <div class="max-w-xs w-full mx-auto">
-        <form
+        <Form
+          action={action}
           class={
-            "flex flex-col space-y-4 p-6 shadow border bg-white rounded-lg"
+            "flex flex-col space-y-2 p-6 shadow border bg-white rounded-lg"
           }
         >
           <div class={"flex flex-col space-y-2"}>
             <label for="name">Name</label>
             <input class={"rounded py-1"} type="text" name="name" id="name" />
+            <span class={"text-sm text-red-500"}>
+              {action.value?.fieldErrors?.name}
+            </span>
           </div>
           <div class={"flex flex-col space-y-2"}>
             <label for="email">Email address</label>
@@ -21,6 +37,9 @@ export default function Signup() {
               name="email"
               id="email"
             />
+            <span class={"text-sm text-red-500"}>
+              {action.value?.fieldErrors?.email}
+            </span>
           </div>
           <div class={"flex flex-col space-y-2"}>
             <label for="password">Password</label>
@@ -30,6 +49,9 @@ export default function Signup() {
               name="password"
               id="password"
             />
+            <span class={"text-sm text-red-500"}>
+              {action.value?.fieldErrors?.password}
+            </span>
           </div>
 
           <button
@@ -39,7 +61,7 @@ export default function Signup() {
           >
             Sign up
           </button>
-        </form>
+        </Form>
 
         <div class={"p-6  text-center border shadow bg-white rounded-lg mt-6"}>
           <span class={"text-gray-500"}>Already have an account?</span>
@@ -51,4 +73,4 @@ export default function Signup() {
       </div>
     </div>
   );
-}
+});
